542f8267eb8fde427739a6a8ee7e5276d6755dd2
arcanaer <carlosvaldes096@gmail.com>
2020-04-23 23:16:21 -0500

    Versión 2, se añade inventario de sprint de semana 3 RN003 Y RN004

---
+++ inventario.php
@@ -0,0 +1,26 @@
+<?php
+include ('functions/header.php');
+include ('functions/functions.php');
+include ('plantilla.php'); 
+  
+?>
+  <!-- Page Content -->
+
+  <div class="container">
+    <div class="row text-center py-5 shadow mt-5 pacientes border-bottom-orange ov-scroll">
+      
+      <div class="col-12 col-md-3 my-2"><button class="btn btn-primary">Producción</button></div>
+      <div class="col-12 col-md-3 my-2"><button class="btn btn-success">Planta</button></div>
+      <div class="col-12 col-md-3 my-2"><button class="btn btn-danger">Herramientas</button></div>
+      <div class="col-12 col-md-3 my-2"><button class="btn btn-secondary">Insumos</button></div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+<?php include('functions/footer.php'); ?>
---
+++ seguimiento.php
@@ -0,0 +1,26 @@
+<?php
+include ('functions/header.php');
+include ('functions/functions.php');
+include ('plantilla.php'); 
+  
+?>
+  <!-- Page Content -->
+
+  <div class="container">
+    <div class="row text-center py-5 shadow mt-5 pacientes border-bottom-orange ov-scroll">
+      
+      <div class="col-12 col-md-3 my-2"><button class="btn btn-primary">Procesos</button></div>
+      <div class="col-12 col-md-3 my-2"><button class="btn btn-success">Actores y roles</button></div>
+      <div class="col-12 col-md-3 my-2"><button class="btn btn-danger">Tiempos de procesos</button></div>
+      <div class="col-12 col-md-3 my-2"><button class="btn btn-secondary">Stoppers</button></div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+<?php include('functions/footer.php'); ?>
